229628cead04ed6f134fb95f9667c97e27a86c2c
zhangyouxin <zhangyouxingg.126.com>
2018-03-08 17:00:07 +0800

    test before test uit

--- test/com/twu/biblioteca/ExampleTest.java
+++ test/com/twu/biblioteca/ExampleTest.java
@@ -1,13 +1,26 @@
 package com.twu.biblioteca;
 
 
+import org.junit.Before;
 import org.junit.Test;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 
 public class ExampleTest {
+    BibliotecaApp app;
+
+    @Before
+    public void resetApp () {
+        app = new BibliotecaApp();
+    }
 
     @Test
     public void test() {
         assertEquals(1, 1);
+    }
+
+    @Test
+    public void testReset() {
+        assertNotNull(app);
     }
 }
